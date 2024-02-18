ca57ca554b53942a7d4c533ec5c5f2b5fd100a33
LookinGit <alook.in@yandex.ru>
2024-02-18 22:12:41 +0500

    fix: fix imports of module

--- index.ts
+++ index.ts
@@ -8,6 +8,4 @@
  */
 
 export { configure } from './configure.js'
-export { stubsRoot } from './stubs/main.js'
-export { BaseModelFilter } from './src/base_model.js'
-export { Filterable } from './src/mixin.js'
+export { SoftDeletes } from './src/mixin.js'
